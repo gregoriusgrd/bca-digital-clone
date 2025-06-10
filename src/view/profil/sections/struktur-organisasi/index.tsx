f6be3c65b7dc632fa2dad1e0d96855ac3aea76d9
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-11 01:31:59 +0700

    struktur organisasi half

--- src/view/profil/sections/struktur-organisasi/index.tsx
+++ src/view/profil/sections/struktur-organisasi/index.tsx
@@ -3,6 +3,11 @@ import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+import Image from "next/image";
+
+import { BsArrowRightCircleFill } from "react-icons/bs";
+import { BsArrowLeftCircleFill } from "react-icons/bs";
+
 export default function StrukturOrganisasiSection() {
   useEffect(() => {
     AOS.init({
@@ -13,11 +18,47 @@ export default function StrukturOrganisasiSection() {
   }, []);
 
   return (
-    <section id="4" className="py-16 bg-[#F4FBFB] text-center">
-      <div className="container mx-auto">
+    <section id="4" className="bg-fuchsia-300 py-16 text-center">
+      <div className="container mx-auto px-6">
+        <h2 className="text-3xl font-bold mb-6 text-white">
+          Struktur Organisasi
+        </h2>
 
+        {/* Organizational Structure Image */}
+        <div className="">
+          <Image 
+          src="/profil/img-structure-organisasi-01.png"
+          alt="Struktur Organisasi BCA"
+          width={888}
+          height={716}
+          className="relative w-full h-full"
+          />
+        </div>
+
+        {/* Buttons */}
+        <div className="flex justify-center mt-6 space-x-4">
+          <button>
+            <BsArrowLeftCircleFill />
+          </button>
+          <button>
+            <BsArrowRightCircleFill />
+          </button>
+        </div>
       </div>
     </section>
   );
 }
 
+/*
+        <div className="absolute bottom-0 right-0 w-[50%] h-full z-0">
+          <Image
+            src="/home/header-home.png"
+            alt="Hero Image"
+            fill
+            className="object-contain"
+            priority
+          />
+        </div>
+
+        <BsArrowLeftCircleFill />
+*/
