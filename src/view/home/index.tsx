31ddfa623e36ae756047fc6d767779238d67e2d0
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-10 20:18:47 +0700

    tata section half done

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,13 +1,15 @@
 "use client"
 
-import HeroSection from "./hero-section"
-import ProductSection from "./product-section"
+import HeroSection from "./sections/hero"
+import ProductSection from "./sections/product"
+import TataSection from "./sections/tata"
 
 export default function HomeView () {
     return (
         <main>
             <HeroSection />
             <ProductSection />
+            <TataSection />
         </main>
     )
 }
---
+++ src/view/home/sections/tata/index.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+
+import { BsArrowRightShort } from "react-icons/bs";
+
+export default function TataSection() {
+  return (
+    <section className="bg-fuchsia-200 h-200 px-30 py-8">
+      <div className="">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 items-center h-full">
+          <div className="img">
+            <Image 
+            src="/home/home-tatakelola.png"
+            alt="Tata Kelola Image"
+            width={477}
+            height={477}
+            className=""
+            />
+          </div>
+          <div className="oo">
+            <div className="box-section">
+              <div className="desktop">
+                <h2 className="text-4xl text-[#00A6B5]">Tata Kelola Perusahaan</h2>
+                <p className="text-[#292929]">Bagi BCA Digital, transparansi usaha, kualitas pelayanan, dan kepercayaan nasabah adalah kunci atas keberlangsungan perusahaan kami.</p>
+                <div className="flex items-center">
+                    <a href="#" className="text-black font-bold">Selengkapnya</a>
+                    <BsArrowRightShort className="text-black text-2xl" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+/*
+          <Image
+            src="/home/header-home.png"
+            alt="Hero Image"
+            fill
+            className="object-contain"
+            priority
+          />
+
+          container mx-auto grid grid-cols-1 md:grid-cols-2 items-center h-full px-6
+*/
