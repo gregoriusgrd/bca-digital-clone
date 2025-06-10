5ba91326b8adb532f0dc1d6458c4b59a3b79e2d5
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-10 18:16:58 +0700

    product section half done

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import HeroSection from "./hero-section"
+import ProductSection from "./product-section"
 
 export default function HomeView () {
     return (
         <main>
             <HeroSection />
+            <ProductSection />
         </main>
     )
 }
---
+++ src/view/home/product-section/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+
+export default function ProductSection() {
+  return (
+    <section className="bg-white h-[725px] px-30 py-8">
+      <div className="m-auto h-full flex flex-col justify-center items-center px-8 py-10">
+        <Image
+          src="/home/logo-product.png"
+          alt="BCA Digital Logo"
+          width={120}
+          height={120}
+          className="object-contain mb-8"
+        />
+        <h2 className="text-3xl font-bold text-[#0D3952] mb-8">blu by BCA Digital</h2>
+        <p className="text-center text-xl text-[#464646] mb-15">Aplikasi mobile bank digital yang bisa ini itu, dan menyatu dengan hidupmu</p>
+        <a href="#" className="text-[#30CED0] border-2 rounded-full border-[#30CED0] px-9 py-5">Selengkapnya</a>
+      </div>
+    </section>
+  );
+}
