96981e54655391fc4a5100eae8a1eeff2735f769
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-17 23:31:59 +0700

    update

--- src/view/laporan/sections/01-hero/index.tsx
+++ src/view/laporan/sections/01-hero/index.tsx
@@ -6,12 +6,14 @@ import Image from "next/image";
 export default function HeroSection() {
   return (
     <>
-      <Navbar />
+      <nav>
+        <Navbar />
+      </nav>
 
-      {/* Hero Section */}
-      <section id="1" className="relative bg-[#12B2C0] h-[600px] overflow-hidden">
-
-        {/* Text Content */}
+      <section
+        id="1"
+        className="relative bg-[#12B2C0] h-[600px] overflow-hidden"
+      >
         <div className="relative z-10 max-w-7xl mx-auto px-16 h-full grid grid-cols-1 md:grid-cols-2 items-center">
           <div className="flex flex-col gap-8 max-w-sm">
             <h1 className="text-white text-7xl font-bold leading-tight">
